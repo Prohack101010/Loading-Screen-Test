9abc5d4481ce6c04cdf362746e7ecbccc5c4398c
Prohack101010 <126958051+Prohack101010@users.noreply.github.com>
2024-06-02 19:59:42 +0300

    Update CustomFadeTransition.hx

--- source/CustomFadeTransition.hx
+++ source/CustomFadeTransition.hx
@@ -40,7 +40,7 @@ class CustomFadeTransition extends MusicBeatSubstate {
 		transGradient.x -= (width - FlxG.width) / 2;
 		transBlack.x = transGradient.x;
 
-		var LoadBF:FlxSprite = new FlxSprite(-200, 350);
+		var LoadBF:FlxSprite = new FlxSprite(-200, 300);
 		LoadBF.frames = Paths.getSparrowAtlas('bf running');
 		LoadBF.animation.addByPrefix('bf running', 'bf running');
 		LoadBF.animation.play('bf running');
